af11dacdb250cd05ec80c32dd75328d9db872448
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2023-04-08 22:17:07 +0700

    feat[DA](home): rebase development branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,26 +3,19 @@
 </svelte:head>
 <script>
 </script>
-<div class="text-3xl font-bold bg-[#202020] min-h-screen text-[#FFF7DA] overflow-hidden">
-    <div class="main slide-in">
-        main
-      </div>
+<div class="font-bold bg-[#202020] text-[#FFF7DA] h-[calc(100vh_-_5rem)] grid place-items-center overflow-hidden">
+    <div class="main flex flex-col mx-auto items-center justify-center">
+        <video autoplay loop muted preload="auto" class="w-1/2 h-2/3 max-:hidden">
+          <source src="src/assets/video/welkam_madani_2d.mp4" type="video/mp4">
+          <track kind="captions" srclang="en" label="English" default>
+        </video>
+        <div class="md:text-3xl xl:text-5xl sm:text-xl min-[2400px]:text-9xl max-[360px]:text-xs">KOMUNITAS GAMERS MUSLIM INDONESIA</div>
+        <div class="text-[#FCCD25] mt-2 min-[350px]:text-sm md:text-xl min-[2400px]:text-5xl text-xs">KUY GABUNG, JADI BAGIAN KOTA MADANI</div>
+        <a href="https://discord.gg/VbGZ8yvhKw" class="bg-[#FCCD25] mt-10 py-5 px-6 text-[#202020] min-[2400px]:text-7xl md:text-xl text-sm">GABUNG SEKARANG</a>
+    </div>
 </div>
 
 <style>
-  /* animation slide in from buttom */
-  .slide-in {
-    animation: slideIn 1s ease-in-out;
-  }
-  @keyframes slideIn {
-    0% {
-      transform: translateY(100%);
-      opacity: 0;
-    }
-    100% {
-      transform: translateY(0);
-      opacity: 1;
-    }
-  }
+  /* animation */
 </style>
 
